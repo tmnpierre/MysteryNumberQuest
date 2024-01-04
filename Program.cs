51eb76b07084f3b075b9bbe6e5911a6e6ab033d1
tmnpierre <tomyn.pierre@gmail.com>
2024-01-04 22:06:24 +0100

    feat: Implement Random Number Generation in Main Method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MysteryNumberQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int mysteryNumber = random.Next(1, 101);
+
+            Console.WriteLine("The mystery number has been generated. Try to guess it!");
+        }
+    }
+}
